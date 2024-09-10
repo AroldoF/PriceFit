87767b112b2bf58912ccaab625babf4ec9f28e9c
Arôldo Fernandes <aroldo.caval@gmail.com>
2024-09-10 16:13:32 -0300

    cupom responsividade quase finalizado

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ function menuShow() {
         document.querySelector('.icon').src = "img/close_white_36dp.svg";
     }
 }
+
+function filtroShow() {
+    let menuMobile = document.querySelector('.cumpons_divs_lojas_escondido'); // Corrigido o seletor
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('icon_cupom').src = "img/close_white_36dp.svg"; // Corrigido para o ícone de filtro
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('icon_cupom').src = "img/close_white_36dp.svg"; // Ícone de fechar
+    }
+}
